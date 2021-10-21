a843970f811d2853a1a143912c1d96066f007e12
Faysal Akbar <faysalrj361@gmail.com>
2021-10-21 20:43:55 +0600

    loaded my own data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const nameRef = useRef();
+  const emailRef = useRef();
+
+  useEffect(()=> {
+    fetch('http://localhost:3000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUser = {name: name, email: email};
+
+    //send data to the server
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+  })
+  .then(res => res.json())
+  .then(data => {
+    const addedUser = data;
+    const newUsers = [...users, addedUser];
+    setUsers(newUsers);
+  })
+  nameRef.current.value = '';
+  emailRef.current.value = '';
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>I have {users.length} users.</h2>
+      <form onSubmit={handleAddUser}>
+        <input ref={nameRef} type="text" placeholder="Name" /><br />
+        <input ref={emailRef} type="email" name="email" id="" placeholder="Email"/><br />
+        <input type="submit" value="submit" />
+      </form>
+      {
+        users.map(user => <h3 key={user.id}>{user.name} {user.email}</h3>)
+      }
     </div>
   );
 }
